0cc28a3c549d636cb32b8b070c249fe6d130d5f4
User Name <ssinco90@gmail.com>
2024-11-04 18:02:03 -0800

    mvp for rank page

--- config/challenges.js
+++ config/challenges.js
@@ -4,8 +4,8 @@ module.exports = {
     { version: 0, rank: 1, challengeId: 1, uid: '0-1-1', description: "1 single count challenge", completed: false, completedAt: null, deprecated: false },
     { version: 0, rank: 1, challengeId: 2, uid: '0-1-2', description: "Hards training", completed: false, completedAt: null, deprecated: false },
     { version: 0, rank: 2, challengeId: 1, uid: '0-2-1', description: "10 single counts", completed: false, completedAt: null, deprecated: false },
-    { version: 0, rank: 2, challengeId: 2, uid: '0-2-2', description: "Pairs and softs training", completed: false, completedAt: null, deprecated: false },
-    { version: 0, rank: 2, challengeId: 3, uid: '0-2-3', description: "Pairs and softs training", completed: false, completedAt: null, deprecated: false },
+    { version: 0, rank: 2, challengeId: 2, uid: '0-2-2', description: "Pairs training", completed: false, completedAt: null, deprecated: false },
+    { version: 0, rank: 2, challengeId: 3, uid: '0-2-3', description: "Softs training", completed: false, completedAt: null, deprecated: false },
     { version: 0, rank: 3, challengeId: 1, uid: '0-3-1', description: "10 single counts, averaging < 2 min", completed: false, completedAt: null, deprecated: false },
     { version: 0, rank: 3, challengeId: 2, uid: '0-3-2', description: "Random training", completed: false, completedAt: null, deprecated: false },
     // Add new challenges as needed
